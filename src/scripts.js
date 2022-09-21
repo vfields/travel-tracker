7d6da5f82d888105714e509a082c31ae8c0b5505
Victoria Fields <victoriaashleyfields@gmail.com>
2022-09-20 23:32:52 -0600

    Create rough display of trips, functional

--- src/scripts.js
+++ src/scripts.js
@@ -18,7 +18,7 @@ Promise.all([fetchData('travelers'), fetchData('trips'), fetchData('destinations
 
 function setData(datasets) {
   travelerDataset = new Dataset(datasets[0]);
-  currentTraveler = new Traveler(travelerDataset.data[6]) // will likely need to be its own function with login functionality
+  currentTraveler = new Traveler(travelerDataset.data[44]) // will likely need to be its own function with login functionality
   tripDataset = new Dataset(datasets[1]);
   currentTraveler.setTravelerTrips(tripDataset, 'userID');
   // console.log('currentTraveler.trips', currentTraveler.trips);
@@ -33,14 +33,59 @@ function setData(datasets) {
 const travelerFirstName = document.querySelector('.traveler-first-name');
 const todaysDate = document.querySelector('.todays-date');
 const travelerTotalSpent = document.querySelector('.total-spent');
+const pastTripsSection = document.querySelector('.past-trips-section');
+const pendingTripsSection = document.querySelector('.pending-trips-section');
+const upcomingTripsSection = document.querySelector('.upcoming-trips-section');
 
-// EVENT HANDLERS *************************************************
+// FUNCTIONS ******************************************************
 function displayTravelerData() {
   displayTravelerInfo();
+  displayTrips();
 }
 
 function displayTravelerInfo() {
   travelerFirstName.innerText = currentTraveler.findFirstName();
   todaysDate.innerText = new Date().toLocaleDateString();
   travelerTotalSpent.innerText = currentTraveler.calcTotalSpent().toFixed(2);
+}
+
+function displayTrips() {
+  const today = new Date().toISOString().slice(0, 10).split('-').join('/');
+  currentTraveler.trips.forEach(trip => {
+    const destination = currentTraveler.destinations.find(destination => trip.destinationID === destination.id);
+    // console.log('destination', destination)
+    // manipulate trip date to display a range of days in second <p>
+    if (trip.status === 'pending') {
+      // console.log('pending trip', trip);
+      pendingTripsSection.innerHTML += `
+      <article class="trip-card" tabindex="0">
+        <p>${destination.destination}</p>
+        <p>${trip.date}</p>
+        <img src="${destination.image}" alt="${destination.alt}">
+      </article>
+      `
+    }
+    else if (trip.date < today) {
+      // console.log('past trip', trip);
+      // past trips
+      pastTripsSection.innerHTML += `
+      <article class="trip-card" tabindex="0">
+        <p>${destination.destination}</p>
+        <p>${trip.date}</p>
+        <img src="${destination.image}" alt="${destination.alt}">
+      </article>
+      `
+    }
+    else {
+      // console.log('upcoming trip', trip);
+      // upcoming trips
+      upcomingTripsSection.innerHTML += `
+      <article class="trip-card" tabindex="0">
+        <p>${destination.destination}</p>
+        <p>${trip.date}</p>
+        <img src="${destination.image}" alt="${destination.alt}">
+      </article>
+      `
+    }
+  })
 }
